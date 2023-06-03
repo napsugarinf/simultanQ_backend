7b8c1e205ec114372765611695e0beff5a6282e1
napsugarinf <napsugarinf@gmail.com>
2023-06-03 16:16:47 +0300

    simultan playing

--- base/src/main/java/com/simultanq/base/websocket/WebSocketQuizController.java
+++ base/src/main/java/com/simultanq/base/websocket/WebSocketQuizController.java
@@ -5,16 +5,26 @@ import com.simultanq.base.entity.dto.QuizDTO;
 import com.simultanq.base.helper.DTOConverters;
 import com.simultanq.base.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 @Controller
+@CrossOrigin(origins = "http://localhost:4200")
 public class WebSocketQuizController {
 
     @Autowired
@@ -25,7 +35,7 @@ public class WebSocketQuizController {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/startQuiz/{quizPin}/{playerId}")
+    @MessageMapping("/startQuiz/{quizPin}")
     public void startQuiz(@DestinationVariable String quizPin) {
         // Retrieve quiz object based on the provided quizPin
         Quiz quiz = quizService.getQuizByPIN(quizPin);
@@ -37,13 +47,16 @@ public class WebSocketQuizController {
         }
     }
 
-    @MessageMapping("/submitResponse/{quizPin}/{playerId}/{questionId}")
-    public void submitResponse(@DestinationVariable String quizPin, @DestinationVariable Long questionId, String response) {
+    @MessageMapping("/submitResponse/{quizPin}/{questionId}")
+    public void submitResponse(@DestinationVariable String quizPin,
+                               @DestinationVariable Long questionId,
+                               String response,
+                               @Header("playerId") String playerId) {
         // Handle the client's response for the given quizPin and questionId
         // You can save the response in the database or perform any necessary processing
 
         // Example: Print the received response
-        System.out.println("Received response for quizPin: " + quizPin + ", questionId: " + questionId);
+        System.out.println("Received response for quizPin: " + quizPin + ", userId: "+ playerId+", questionId: " + questionId);
         System.out.println("Response: " + response);
 
         // Get the next question based on the current questionId
@@ -53,10 +66,10 @@ public class WebSocketQuizController {
             // Send the next question to the corresponding quiz room
             messagingTemplate.convertAndSend("/quiz/" + quizPin, nextQuestion);
 
-    } else {
-        // Last question, send a special message indicating the end of the quiz
-        messagingTemplate.convertAndSend("/quiz/" + quizPin, "Quiz completed");
-    }
+        } else {
+            // Last question, send a special message indicating the end of the quiz
+            messagingTemplate.convertAndSend("/quiz/" + quizPin, "Quiz completed");
+        }
     }
 
     private Question getNextQuestion(Quiz quiz, Long currentQuestionId) {
@@ -74,28 +87,4 @@ public class WebSocketQuizController {
         return null;
     }
 
-//    @MessageMapping("/startQuiz/{quizPin}")
-//    @SendTo("/quiz/{quizPin}")
-//    public Quiz startQuiz(@DestinationVariable String quizPin) {
-//        // Retrieve quiz object based on the provided quizPin
-//        Quiz quiz = quizService.getQuizByPIN(quizPin);
-//        QuizDTO quizDTO = DTOConverters.convertQuizToDTO(quiz);
-//        //System.out.println(quizDTO);
-//        // Perform necessary actions to start the quiz
-//        return quiz;
-//    }
-
-
-//    @MessageMapping("/quiz/{quizPin}/results")
-//    @SendTo("/quiz/{quizPin}/results")
-//    public QuestionResult handleResults(@DestinationVariable String quizPin, QuestionResult result) {
-//        // Process and store the question result
-//
-//
-//        //Store the result in memory
-//        quizResults.computeIfAbsent(quizPin, k -> new ArrayList<>()).add(result);
-//
-//        // Return the processed result to be broadcasted to all connected players
-//        return result;
-//    }
 }
