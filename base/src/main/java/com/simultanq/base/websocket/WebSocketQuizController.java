56ecffd0e7d9a82cc0b29736f3cb55a5e19f61cd
napsugarinf <napsugarinf@gmail.com>
2023-06-30 00:00:24 +0300

    dtos

--- base/src/main/java/com/simultanq/base/websocket/WebSocketQuizController.java
+++ base/src/main/java/com/simultanq/base/websocket/WebSocketQuizController.java
@@ -1,6 +1,7 @@
 package com.simultanq.base.websocket;
 import com.simultanq.base.entity.Question;
 import com.simultanq.base.entity.Quiz;
+import com.simultanq.base.entity.dto.QuestionDTO;
 import com.simultanq.base.entity.dto.QuizDTO;
 import com.simultanq.base.helper.DTOConverters;
 import com.simultanq.base.service.QuestionService;
@@ -19,9 +20,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -47,11 +46,25 @@ public class WebSocketQuizController {
 
 //    //private Map<String, Integer> participants = new HashMap<>();
 
+
+//    @MessageMapping("/joinQuiz/{quizPin}/{playerId}")
+//    public void joinQuiz(@DestinationVariable String quizPin,
+//                          @Header("playerId") String playerId) {
+//        participants.clear();
+//        int score = 0;
+//
+//        if (!participants.containsKey(quizPin)) {
+//            participants.put(quizPin, new HashMap<>());
+//        }
+//        sendparticipants(quizPin);
+//        participants.get(quizPin).put(playerId, score);
+//
+//
+//    }
     @MessageMapping("/startQuiz/{quizPin}/{playerId}")
     public void startQuiz(@DestinationVariable String quizPin,
                           @Header("playerId") String playerId) {
-
-        Quiz quiz = quizService.getQuizByPIN(quizPin);
+        Quiz quiz1 = quizService.getQuizByPIN(quizPin);
         int score = 0;
 
         if (!participants.containsKey(quizPin)) {
@@ -63,9 +76,10 @@ public class WebSocketQuizController {
         sendparticipants(quizPin);
         startQuizTimer(quizPin);
 
-        List<Question> questions = quiz.getQuestions();
+        List<Question> questions = quiz1.getQuestions();
         if (!questions.isEmpty()) {
-            messagingTemplate.convertAndSend("/quiz/" + quizPin + "/" + playerId, questions.get(0));
+            QuestionDTO questionToBeSent = DTOConverters.convertQuestionToDTO(questions.get(0));
+            messagingTemplate.convertAndSend("/quiz/" + quizPin + "/" + playerId, questionToBeSent);
         }
     }
 
@@ -74,8 +88,9 @@ public class WebSocketQuizController {
                                @DestinationVariable Long questionId,
                                String response,
                                @Header("playerId") String playerId) {
-        Quiz quiz = quizService.getQuizByPIN(quizPin);
+        Quiz quiz1 = quizService.getQuizByPIN(quizPin);
         Question question = questionService.getQuestionById(questionId);
+
         boolean isCorrect = checkResponse(question, response);
         int points = isCorrect ? 1 : 0;
 
@@ -83,16 +98,33 @@ public class WebSocketQuizController {
 
         sendparticipants(quizPin);
 
-        Question nextQuestion = getNextQuestion(quiz, questionId);
+        Question nextQuestion = getNextQuestion(quiz1, questionId);
         if (nextQuestion != null) {
-            messagingTemplate.convertAndSend("/quiz/" + quizPin + "/" + playerId, nextQuestion);
+            QuestionDTO nextQuestionToBeSent = DTOConverters.convertQuestionToDTO(nextQuestion);
+            messagingTemplate.convertAndSend("/quiz/" + quizPin + "/" + playerId, nextQuestionToBeSent);
         } else {
             messagingTemplate.convertAndSend("/quiz/" + quizPin + "/" + playerId, "Quiz completed");
+
+
         }
     }
 
     @MessageMapping("/sendparticipants/{quizPin}")
     public void sendparticipants(@DestinationVariable String quizPin) {
+//        Map<String, Integer> quizParticipants = participants.getOrDefault(quizPin, new HashMap<>());
+//
+//        // Sort participants by score in descending order
+//        List<Map.Entry<String, Integer>> sortedParticipants = new ArrayList<>(quizParticipants.entrySet());
+//        sortedParticipants.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+//
+//        // Create a LinkedHashMap to maintain the sorted order
+//        Map<String, Integer> sortedParticipantsMap = new LinkedHashMap<>();
+//        for (Map.Entry<String, Integer> entry : sortedParticipants) {
+//            sortedParticipantsMap.put(entry.getKey(), entry.getValue());
+//        }
+//
+//
+//        messagingTemplate.convertAndSend("/quiz/" + quizPin, sortedParticipantsMap);
         messagingTemplate.convertAndSend("/quiz/" + quizPin, participants.get(quizPin));
     }
 
@@ -105,8 +137,6 @@ public class WebSocketQuizController {
             }
         }
     }
-
-
 
 
     private Question getNextQuestion(Quiz quiz, Long currentQuestionId) {
@@ -124,50 +154,42 @@ public class WebSocketQuizController {
         return null;
     }
 
-
-
     private boolean checkResponse(Question question, String response) {
         Long correctAnswerId = question.getCorrectAnswerId();
         // Compare the response with the correct answer's ID
         return response.equals(String.valueOf(correctAnswerId));
     }
 
-    private void startQuizTimer(String quizPin) {
-        if (!quizTimers.containsKey(quizPin)) {
-            long currentTime = System.currentTimeMillis();
-            quizTimers.put(quizPin, currentTime);
-        }
+
+    @MessageMapping("/endQuiz/{quizPin}")
+    private void endQuiz(String quizPin) {
+        participants.remove(quizPin);
+        messagingTemplate.convertAndSend("/quiz/" + quizPin, participants);
+
     }
 
+
+
+//    @Scheduled(fixedDelay = 1000) // Runs every second
+//    public void checkQuizTimeLimits() {
+//        for (String quizPin : quizTimers.keySet()) {
+//            long quizStartTime = quizTimers.get(quizPin);
+//            long currentTime = System.currentTimeMillis();
+//            long elapsedTimeInSeconds = (currentTime - quizStartTime) / 1000;
+//
+//            if (elapsedTimeInSeconds >= 30) { // Time limit is 2 minutes (120 seconds)
+//                endQuiz(quizPin);
+//            }
+//        }
+//    }
+private void startQuizTimer(String quizPin) {
+    if (!quizTimers.containsKey(quizPin)) {
+        long currentTime = System.currentTimeMillis();
+        quizTimers.put(quizPin, currentTime);
+    }
+}
     @Bean // Add this bean to the configuration class
     public ScheduledExecutorService quizTimerExecutor() {
         return Executors.newScheduledThreadPool(1);
     }
-
-    @Scheduled(fixedDelay = 1000) // Runs every second
-    public void checkQuizTimeLimits() {
-        for (String quizPin : quizTimers.keySet()) {
-            long quizStartTime = quizTimers.get(quizPin);
-            long currentTime = System.currentTimeMillis();
-            long elapsedTimeInSeconds = (currentTime - quizStartTime) / 1000;
-
-            if (elapsedTimeInSeconds >= 30) { // Time limit is 2 minutes (120 seconds)
-                endQuiz(quizPin);
-            }
-        }
-    }
-    private void endQuiz(String quizPin) {
-        // Perform any necessary actions when the quiz time limit is reached
-        // For example, you can notify participants that the quiz has ended
-        // and calculate final scores or perform any other logic you need.
-        // You can access the participants using participants.get(quizPin) and
-        // perform any required operations.
-        messagingTemplate.convertAndSend("/quiz/" + quizPin, "Quiz over");
-        // Clear the participants and timer for the quiz
-       // participants.remove(quizPin);
-        //quizTimers.remove(quizPin);
-    }
-
-
-
 }
